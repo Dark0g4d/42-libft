9fefe98811c33248b2cf0149bc2f3d8d2463c65e
dark0g4d <dark0g4d7@gmail.com>
2024-11-11 23:54:42 +0000

    make lib and strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcerquei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 23:03:10 by gcerquei          #+#    #+#             */
+/*   Updated: 2024/11/11 23:38:56 by gcerquei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char *str;
+    size_t  i;
+
+    i = -1;
+    if (s == NULL || f = NULL)
+        return (NULL);
+    str = ft_strdup(s);
+    if (str == NULL)
+        return (NULL);
+    while (str[++i])
+        str[i] = f(i, str[i]);
+    return (str);
+}
