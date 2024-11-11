45514841e227d008826300d28da8c80ee0e2fff8
dark0g4d <dark0g4d7@gmail.com>
2024-11-11 23:57:15 +0000

    libft strmapi norm

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,24 +6,24 @@
 /*   By: gcerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 23:03:10 by gcerquei          #+#    #+#             */
-/*   Updated: 2024/11/11 23:38:56 by gcerquei         ###   ########.fr       */
+/*   Updated: 2024/11/11 23:55:50 by gcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    char *str;
-    size_t  i;
+	char *str;
+	size_t i;
 
-    i = -1;
-    if (s == NULL || f = NULL)
-        return (NULL);
-    str = ft_strdup(s);
-    if (str == NULL)
-        return (NULL);
-    while (str[++i])
-        str[i] = f(i, str[i]);
-    return (str);
+	i = -1;
+	if (s == NULL || f == NULL)
+		return (NULL);
+	str = ft_strdup(s);
+	if (str == NULL)
+		return (NULL);
+	while (str[++i])
+		str[i] = f(i, str[i]);
+	return (str);
 }
