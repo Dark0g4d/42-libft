ffc00558f7cd55cc0d52a11d02c049c85fd97de7
dark0g4d <dark0g4d7@gmail.com>
2024-11-12 01:24:19 +0000

    no leak split

--- ft_split.c
+++ ft_split.c
@@ -5,21 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/10 20:31:41 by gcerquei          #+#    #+#             */
-/*   Updated: 2024/11/11 03:43:22 by gcerquei         ###   ########.fr       */
+/*   Created: 2024/11/12 01:17:33 by gcerquei          #+#    #+#             */
+/*   Updated: 2024/11/12 01:23:36 by gcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	free_all(char **str)
+static void	free_all(char **strarray, int j)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-		free(str[i++]);
-	free(str);
+	while (j > 0)
+		free(strarray[--j]);
+	free(strarray);
 }
 
 static int	count_words(const char *s, char c)
@@ -45,44 +42,47 @@ static int	count_words(const char *s, char c)
 	return (words);
 }
 
-static void	split(char const *s, char c, char **strarray)
+static int	split(char const *s, char c, char **strarray)
 {
 	int	i;
 	int	j;
 	int	start;
-	int	words;
 
 	i = 0;
 	j = 0;
-	start = 0;
-	words = count_words(s, c);
-	while (j < words)
+	while (s[i])
 	{
 		while (s[i] == c)
 			i++;
+		if (s[i] == '\0')
+			break ;
 		start = i;
 		while (s[i] && s[i] != c)
 			i++;
 		strarray[j] = ft_substr(s, start, i - start);
 		if (strarray[j] == NULL)
 		{
-			free_all(strarray);
-			return ;
+			free_all(strarray, j);
+			return (0);
 		}
 		j++;
 	}
 	strarray[j] = NULL;
+	return (1);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**strarray;
+	int		words;
 
 	if (s == NULL)
 		return (NULL);
-	strarray = malloc((count_words(s, c) + 1) * sizeof(char *));
+	words = count_words(s, c);
+	strarray = malloc((words + 1) * sizeof(char *));
 	if (strarray == NULL)
 		return (NULL);
-	split(s, c, strarray);
+	if (split(s, c, strarray) == 0)
+		return (NULL);
 	return (strarray);
 }
