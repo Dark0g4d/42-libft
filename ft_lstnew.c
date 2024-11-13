acdd96d34039ae65bc4d5351105545fb9899cf05
dark0g4d <dark0g4d7@gmail.com>
2024-11-13 00:55:32 +0000

    norm

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,20 +6,20 @@
 /*   By: gcerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 21:16:27 by gcerquei          #+#    #+#             */
-/*   Updated: 2024/11/12 21:39:10 by gcerquei         ###   ########.fr       */
+/*   Updated: 2024/11/13 00:53:51 by gcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list  *ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
-    t_list  *new;
+	t_list	*new;
 
-    new = malloc(sizeof(t_list));
-    if (new == NULL)
-        return (NULL);
-    new->content = content;
-    new->next = NULL;
-    return (new);
+	new = malloc(sizeof(t_list));
+	if (new == NULL)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
 }
