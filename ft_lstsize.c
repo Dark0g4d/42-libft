1a7c9e4c412740f9f5003adfe5cbe3124bea1d47
Dark0g4d <dark0g4d7@gmail.com>
2024-11-17 17:09:08 +0000

    lstsize

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: gcerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 01:08:46 by gcerquei          #+#    #+#             */
-/*   Updated: 2024/11/13 01:23:23 by gcerquei         ###   ########.fr       */
+/*   Updated: 2024/11/17 17:07:54 by gcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,16 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (i);
 }
+/*
+int	main(void)
+{
+	t_list	*node_1 = ft_lstnew("banana");
+	t_list	*node_2 = ft_lstnew("banana");
+
+	node_1->next = node_2;
+	node_2->next = NULL;
+	printf("size: %i\n", ft_lstsize(node_1));
+	free(node_1);
+	free(node_2);
+}
+*/
