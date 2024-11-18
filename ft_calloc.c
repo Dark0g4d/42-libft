edfef495dc0d5559f39b62aaac29166aeaad3d58
dark0g4d <dark0g4d7@gmail.com>
2024-11-18 16:19:07 +0000

    fixed calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,11 +16,14 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
+	if (size == 0)
+		return (malloc(0));
 	if (nmemb > (size_t)-1 / size)
 		return (NULL);
 	ptr = malloc(nmemb * size);
-	if (ptr)
-		ft_bzero(ptr, nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, nmemb * size);
 	return (ptr);
 }
 /*
