a307db841d6d610fb3fd888e26472d63b3c2d4fb
Gabriel Cerqueira Faddaoui <gcerquei@c1r10s4.42lisboa.com>
2024-11-19 14:42:49 +0000

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: gcerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 13:03:34 by gcerquei          #+#    #+#             */
-/*   Updated: 2024/11/17 01:31:00 by gcerquei         ###   ########.fr       */
+/*   Updated: 2024/11/19 14:42:07 by gcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,9 @@ int	ft_atoi(const char *nptr)
 	while (nptr[i] == ' ' || (nptr[i] >= '\t' && nptr[i] <= '\r'))
 		i++;
 	j = i + 1;
-	while (nptr[i] == '+' || nptr[i] == '-')
+	if (nptr[i] == '+' || nptr[i] == '-')
 		if (nptr[i++] == '-')
 			sign *= -1;
-	if (i > j)
-		return (0);
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 		result = 10 * result + (nptr[i++] - '0');
 	return (result * sign);
