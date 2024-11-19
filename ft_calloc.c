1522ef38d4c1b023f70fa43490a79fafa75c7e81
Gabriel Cerqueira Faddaoui <gcerquei@c1r10s4.42lisboa.com>
2024-11-19 15:00:24 +0000

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: gcerquei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 19:55:33 by gcerquei          #+#    #+#             */
-/*   Updated: 2024/11/17 01:30:41 by gcerquei         ###   ########.fr       */
+/*   Updated: 2024/11/19 14:58:49 by gcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,9 @@ int main(void)
 	char	*r_calloc;
 	char	*r_ftcalloc;
 
+	printf("%lu\n", (size_t) - 1);
 	r_calloc = calloc(0, 0);
-	r_ftcalloc = ft_calloc(0, 0);
+	r_ftcalloc = ft_calloc(10, 10);
 	printf("libc: %s\n", r_calloc);
 	printf("libft: %s\n", r_ftcalloc);
 }
